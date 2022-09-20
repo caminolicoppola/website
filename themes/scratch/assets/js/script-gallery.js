f5c869c2ef7a8156ff9b74d54abfcb6e3a364d9a
lucalandi <hello@luca.land>
2022-09-20 15:51:25 +0200

    Fixes

--- themes/scratch/assets/js/script-gallery.js
+++ themes/scratch/assets/js/script-gallery.js
@@ -16,6 +16,9 @@ $( document ).ready(function() {
     $('.gallery-guide--right').click(goRight);
     $('.gallery-guide--left').click(goLeft);
 
+    $(document).on('swiped-left', $container, function() {goRight()})
+    $(document).on('swiped-right', $container, function() {goLeft()})
+
     function goLeft() {
         $container.stop(true,true); //complete any animation still running  
         var $newLeft = $container.position().left+(1*$imgWidth);//calculate the new position which is the current position plus the width of one image
